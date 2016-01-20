96980c2d953b8d69d37852a72e97c1c345b78a2e
PARTHASARATHY MURALI <PARTHASARATHY MURALI>
2016-01-20 13:57:20 +0530

    exercise 5 completed

---
+++ exercise_5.rb
@@ -0,0 +1,16 @@
+my_name = 'Parthasarathy M'
+my_age = 38 # not a lie in 2009
+my_height = 5.9 # inches
+my_weight = 68 # kgs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} kgs normal."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
